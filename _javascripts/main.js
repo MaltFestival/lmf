3b971eb82d8b0d928778e9f74e9127efb6b4c443
Ludovic Turmel <ludovic.turmel@liip.ch>
2018-12-27 17:59:41 +0100

    Flashback

--- _javascripts/main.js
+++ _javascripts/main.js
@@ -1,1 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {});
+import Slider from './components/slider';
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (document.querySelector('body').classList.contains('home')) {
+    new Slider(document.querySelectorAll('.swiper-container'));
+  }
+});
